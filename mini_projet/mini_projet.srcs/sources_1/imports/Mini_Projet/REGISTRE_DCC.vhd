c1b426a35c0c6675076fb745e082cb9cdf729c17
Giorgio Mendieta <31053658+GiorgioMendieta@users.noreply.github.com>
2024-03-13 12:10:02 +0100

    Whitespace changes

--- mini_projet/mini_projet.srcs/sources_1/imports/Mini_Projet/REGISTRE_DCC.vhd
+++ mini_projet/mini_projet.srcs/sources_1/imports/Mini_Projet/REGISTRE_DCC.vhd
@@ -17,10 +17,8 @@
 -- Additional Comments:
 -- 
 ----------------------------------------------------------------------------------
-
-
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_1164.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -32,37 +30,38 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity REGISTRE_DCC is
-  Port ( 
-        clk: in std_logic;
-        reset: in std_logic;
-        COM_REG: in std_logic_vector(1 downto 0);
-        bit_out: out std_logic;
-        Trame:  in std_logic_vector(50 downto 0)
+  port
+  (
+    clk     : in std_logic;
+    reset   : in std_logic;
+    COM_REG : in std_logic_vector(1 downto 0);
+    bit_out : out std_logic;
+    Trame   : in std_logic_vector(50 downto 0)
   );
 end REGISTRE_DCC;
 
 architecture Behavioral of REGISTRE_DCC is
-    signal shift_register : std_logic_vector(50 downto 0);
+  signal shift_register : std_logic_vector(50 downto 0);
 
 begin
-        
-    process (clk, reset)
-    begin
-        -- Reset Asynchrone
-        if reset = '1' then 
-            bit_out <= '0';
-            shift_register <= (others => '0');
-            
-        elsif rising_edge(clk) then
-            if COM_REG(1) = '1' then
-                shift_register <= Trame;
-                
-            elsif COM_REG(0) = '1' then
-                bit_out <= shift_register(50);
-                shift_register <= shift_register(49 downto 0) & '0';
-            end if;
-             
-        end if;
-    end process;
+
+  process (clk, reset)
+  begin
+    -- Reset Asynchrone
+    if reset = '1' then
+      bit_out        <= '0';
+      shift_register <= (others => '0');
+
+    elsif rising_edge(clk) then
+      if COM_REG(1) = '1' then
+        shift_register <= Trame;
+
+      elsif COM_REG(0) = '1' then
+        bit_out        <= shift_register(50);
+        shift_register <= shift_register(49 downto 0) & '0';
+      end if;
+
+    end if;
+  end process;
 
 end Behavioral;
