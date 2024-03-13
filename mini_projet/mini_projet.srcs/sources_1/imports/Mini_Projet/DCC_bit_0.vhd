36a6b2d4e657a74f8086e017649f540e26cedcb1
Giorgio Mendieta <31053658+GiorgioMendieta@users.noreply.github.com>
2024-03-13 11:13:36 +0100

    Fix bugs with DCC bit generation

--- mini_projet/mini_projet.srcs/sources_1/imports/Mini_Projet/DCC_bit_0.vhd
+++ mini_projet/mini_projet.srcs/sources_1/imports/Mini_Projet/DCC_bit_0.vhd
@@ -1,3 +1,5 @@
+-- Note: Owen m'a dit qu'il commence à compter dans les états High et Low
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 use IEEE.numeric_std.all;
@@ -5,83 +7,99 @@ use IEEE.numeric_std.all;
 entity DCC_bit_0 is
   port
   (
-    Reset    : in std_logic;  -- Reset Asynchrone
-    Clk_In   : in std_logic;  -- Horloge 100 MHz de la carte Nexys
-    Clk_1Mhz : in std_logic;  -- Horloge 1 MHz donnée par Tempo
-    Go       : in std_logic;  -- Signal de Validation
-    Fin      : out std_logic; -- Signal de Fin
-    DCC_Out  : out std_logic  -- Signal DCC de sortie
+    Reset     : in std_logic;  -- Reset Asynchrone
+    Clk       : in std_logic;  -- Horloge 100 MHz de la carte Nexys
+    Tempo_clk : in std_logic;  -- Horloge 1 MHz donnée par Tempo
+    Go        : in std_logic;  -- Signal de Validation
+    Fin       : out std_logic; -- Signal de Fin
+    DCC_Out   : out std_logic  -- Signal DCC de sortie
   );
 end DCC_bit_0;
 
 architecture Behavioral of DCC_bit_0 is
-  constant MAX_COUNT : integer := 100; -- Durée de la signal (100 microsecondes)
-  type STATE is (Idle, High, Low, Fin);
+  constant MAX_COUNT : integer := 100; -- Signal period (100 microseconds)
+  type STATE is (Idle, High, Low, Finished);
 
-  -- Signaux Internes
-  signal EP, EF : STATE; -- EP: Etat présent, EF: État Futur
-  signal Cpt    : natural range 0 to (MAX_COUNT - 1);
-  signal Done   : std_logic;
+  -- Internal signals
+  signal EP, EF  : STATE; -- EP: Etat présent, EF: État Futur
+  signal Cpt     : integer range 0 to (MAX_COUNT - 1);
+  signal Clk_DCC : std_logic;
 
 begin
 
-  Clk_Out <= Clk_Temp; -- Affectation du Port de Sortie
-
   -- Processus de Synchronisation du MAE
-  sync : process (Clk_In, Reset)
+  sync : process (Clk, Reset)
   begin
     if (Reset = '0') then
-      EP <= Reset;
-    elsif (rising_edge(Clk_In)) then
+      EP <= Idle;
+    elsif (rising_edge(Clk)) then
       EP <= EF;
     end if;
   end process; -- Sync
 
   -- Processus de Comptage cadencé par l'horloge 1 MHz
-  Count : process (Clk_1Mhz, Reset)
+  Count : process (Tempo_clk, Reset)
   begin
     if (Reset = '0') then
       Cpt <= 0;
-    elsif (rising_edge(Clk_1Mhz)) then
-      Cpt <= Cpt + 1;
-      if Cpt = (MAX_COUNT - 1) then -- Éviter le dépassement
+      -- Clk_DCC <= '0';
+    elsif (rising_edge(Tempo_clk)) then
+      if Cpt = (MAX_COUNT - 1) then
         Cpt <= 0;
+      else
+        Cpt <= Cpt + 1;
+        -- Clk_DCC <= not Clk_DCC;
       end if;
     end if;
   end process; -- Count
 
-  -- Machine à État Fini
-  MAE : process (EP, Go)
+  -- Finite State Machine logic
+  MAE : process (EP, Go, Clk_DCC)
   begin
     case EP is
       when Idle =>
+        -- Next State Logic
         EF <= Idle;
         if (Go = '1') then
+          EF <= Low;
+        end if;
+        -- Output Logic
+        DCC_Out <= '0';
+        Fin     <= '0';
+
+      when Low =>
+        -- Next State Logic
+        EF <= Low;
+        if (rising_edge(Clk_DCC)) then
+          -- if (Clk_DCC = '1') then
           EF <= High;
         end if;
-        Done    <= '0';
+        -- Output Logic
         DCC_Out <= '0';
+        Fin     <= '0';
 
       when High =>
+        -- Next State Logic
         EF <= High;
-        if (Done = '1') then
-          EF   <= Low;
-          Done <= '0';
+        if (rising_edge(Clk_DCC)) then
+          -- if (Clk_DCC = '1') then
+          EF <= Finished;
         end if;
+        -- Output Logic
         DCC_Out <= '1';
-      when Low =>
-        EF <= Low;
-        if (Done = '1') then
-          EF   <= Fin;
-          Done <= '0';
-        end if;
-        DCC_Out <= '0';
+        Fin     <= '0';
 
-      when Fin =>
-        EF      <= Idle;
+      when Finished =>
+        -- Next State Logic
+        EF <= Idle;
+        -- Output Logic
         DCC_Out <= '0';
+        Fin     <= '1';
 
     end case;
   end process; -- MAE
+
+  Clk_DCC <= '1' when (Cpt = (MAX_COUNT - 1)) else
+    '0';
 
 end Behavioral;
